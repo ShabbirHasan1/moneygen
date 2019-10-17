48e4cdc94a01a07cacba7a95b487787cd01de1f6
Mayank Gupta <techfreakworm@gmail.com>
2019-10-17 16:34:11 +0530

    Correct docstring

--- betaNSE/index_derivative_historical.py
+++ betaNSE/index_derivative_historical.py
@@ -8,26 +8,30 @@ class IndexDerivativeHistorical:
     - Nifty 50 (NIFTY)
     - Nifty Bank (BANKNIFTY)
     - Nifty IT (NIFTYIT)
+    Parameters
+    ----------
+    symbol_name : str
+        Symbol of the index.
     """
-    def __init__(self, symbol_name: str, derivative_type: str, expiry_date: str,option_type: str=None ,strike_price: str=None):
-        """
-        Parameters
-        ----------
-        symbol_name: str
-            Symbol of the index.
-        derivative_type: str
-            Can only be either "OPTIONS" or "FUTURES"
-        option_type: str
-            Can only be either "PUT" or "CALL".
-            Required if the "derivative_type"="OPTIONS"
-        expiry_date: str
-            Should be in format "DD-MMM-YYYY"
-        strike_price: str
-            The strike price for contract.
-            Required if the "derivative_type"="OPTIONS"
-        """
+    def __init__(self, symbol_name : str):
         self.driver = SeleniumDispatcher().get_driver();
         self.driver.get(Config.HISTORICAL_DERIVATIVE_DATA_URL + symbol_name.upper())
+        index_select = self.driver.find_element_by_id("eq-derivatives-historical-instrumentType")
+
+        #####self.futures_years =###
+        
+        self.options_years = ###
+        self.futures_expiry_dates = ###
+        self.options_expiry_dates = #####
+        self.options_strike_prices = 
+
+
+    def get_futures_data(self, expiry_date: str, current_year: bool = False):
+        pass
+
+    def get_options_data(self, expiry_date: str, option_type: str, strike_price: str, current_year: bool = False)
+        pass
+
 
     def __del__(self):
         self.driver.close()
