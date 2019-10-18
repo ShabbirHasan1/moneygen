2a8190e583691263f72fb101fd71e94d7f01774c
Mayank Gupta <techfreakworm@gmail.com>
2019-10-18 11:10:22 +0530

    Linting

--- betaNSE/config.py
+++ betaNSE/config.py
@@ -1,5 +1,7 @@
 import sys
 import os
+
+
 class Config(object):
     SELENIUM_DRIVER_BASE_PATH = 'chromedriver'
     SELENIUM_DRIVER_EXEC_PATH = os.path.join(SELENIUM_DRIVER_BASE_PATH, sys.platform)
