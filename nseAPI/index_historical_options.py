1074ab8eeca494395e12bce64f01b160b3303007
Mayank Gupta <techfreakworm@gmail.com>
2019-10-23 17:09:29 +0530

    Expiry-StrikePrice map working

--- nseAPI/index_historical_options.py
+++ nseAPI/index_historical_options.py
@@ -3,29 +3,48 @@ import requests
 
 
 class IndexHistoricalOptions(IndexHistorical):
-    def __init__(self, symbol_name: str):
+    def __init__(self, symbol_name: str, option_type: str):
         super().__init__(symbol_name)
-        self.option_type = 'OPTIONS'
-        self.option_type_val = 'OPTIDX'
-        self.option_type_display_val = 'Index Options'
+        self.derivative_type = 'OPTIONS'
+        self.derivative_type_val = 'OPTIDX'
+        self.derivative_type_display_val = 'Index Options'
+        self.option_type = option_type
 
     def get_expiries(self):
-        res = requests.get(
-            'https://nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteDataTest.jsp?i='\
-            + self.option_type\
-            + '&u='\
-            + self.symbol_name\
-            + '&e=&o=&k='
-        )
+        url = 'https://nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteDataTest.jsp?i='\
+                + self.derivative_type\
+                + '&u='\
+                + self.symbol_name\
+                + '&e=&o=&k='
+        res = requests.get(url)
         self.expiries = res.json()['expiries']
         return self.expiries
 
 
-    def get_strike_prices(self, expiries: list, option_type: str):
-        pass
+    def get_strike_prices(self, expiries: list):
+        expiry_strike_price_map = dict()
+        for expiry in expiries:
+            url = 'https://nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteDataTest.jsp?i='\
+                    + self.derivative_type_val\
+                    + '&u='\
+                    + self.symbol_name\
+                    + '&e='\
+                    + expiry\
+                    + '&o='\
+                    + self.option_type\
+                    + '&k='\
+                    + self.option_type
+            res = requests.get(url)
+            expiry_strike_price_map[expiry] = res.json()['strikePrices']
+        return expiry_strike_price_map
 
-    def get_expiry_strike_price_map(self, expiries: list):
-        pass
+    def get_expiry_strike_price_map_for_all(self):
+        try:
+            self.expiry_strike_price_map = self.get_strike_prices(self.expiries)
+            return self.expiry_strike_price_map
+        except Exception:
+            self.get_expiries()
+            self.get_expiry_strike_price_map_for_all()
 
     def fetch_all_infos(self, expiry: str, strike_price: str):
         pass
--- nseAPI/main.py
+++ nseAPI/main.py
@@ -1,5 +1,6 @@
 from index_historical_options import IndexHistoricalOptions
 
-index_historical_options = IndexHistoricalOptions('BANKNIFTY')
+index_historical_options = IndexHistoricalOptions('BANKNIFTY', 'CE')
 expiries = index_historical_options.get_expiries()
 print(expiries)
+print(index_historical_options.get_strike_prices(expiries))
