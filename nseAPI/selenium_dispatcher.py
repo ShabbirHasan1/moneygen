dc855df8902f2cde6cdc071e71f54c402e84d339
Mayank Gupta <techfreakworm@gmail.com>
2019-10-24 12:17:11 +0530

    Add Selenium dispatcher

---
+++ nseAPI/selenium_dispatcher.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options  
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from config import Config
+
+
+class SeleniumDispatcher:
+    def __init__(self, headless: bool = False, download_path: str = None):
+        # Selenium __driver options for chrome
+        options = Options() 
+        # Enable downloads if download_path is provided
+        if download_path:
+            print('Download is turned on')
+            options.add_experimental_option("prefs", {
+            "download.default_directory": download_path,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+            })
+
+            params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
+
+        # Set runtime config
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--start-maximized")
+        if headless:
+            options.add_argument("--headless")
+
+        self.__driver = webdriver.Chrome(executable_path = Config.SELENIUM_DRIVER_EXEC_PATH, chrome_options = options)
+
+        if download_path:
+            self.__driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+            self.__driver.execute("send_command", params)
+
+    def get_driver(self):
+        return self.__driver
+
+    
