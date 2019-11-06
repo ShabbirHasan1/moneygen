823caea5f0c03d93d1bf7ae31a1a2df3e7193b2a
Mayank Gupta <techfreakworm@gmail.com>
2019-11-07 00:47:21 +0530

    Install chromium browser and  get call options data

--- nseAPI/main.py
+++ nseAPI/main.py
@@ -1,6 +1,6 @@
 from index_historical_options import IndexHistoricalOptions
 
-index_historical_options_call = IndexHistoricalOptions('BANKNIFTY', 'PE')
+index_historical_options_call = IndexHistoricalOptions('BANKNIFTY', 'CE')
 index_historical_options_call.get_info_all()
 # index_historical_options_put = IndexHistoricalOptions('BANKNIFTY', 'PE')
 # print(index_historical_options_put.download_data_all())
--- nseAPI/setup.sh
+++ nseAPI/setup.sh
@@ -3,3 +3,4 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+sudo apt-get install -y chromium-browser
