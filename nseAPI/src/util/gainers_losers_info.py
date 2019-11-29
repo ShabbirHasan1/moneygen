a3667b1c32f033a265d291e222bb15b28cbc1e69
Mayank Gupta <techfreakworm@gmail.com>
2019-11-29 10:32:09 +0530

    Create util module

---
+++ nseAPI/src/util/gainers_losers_info.py
@@ -0,0 +1,12 @@
+class GainersLosersInfo(object):
+    def __init__(self):
+        pass
+
+    def get_instruments(self):
+        pass
+
+    def get_info_specific(self):
+        pass
+
+    def get_info_all(self):
+        pass
