4c9969db61cc74f95500fcc0f7f27912cba9fafa
Mayank Gupta <techfreakworm@gmail.com>
2019-12-02 18:10:19 +0530

    Format output for logging

--- nseAPI/src/main.py
+++ nseAPI/src/main.py
@@ -19,10 +19,22 @@ def nse_india():
     gainers_info = NSEIndiaGLScraper(info_type='Gainers', view_type='All Securities')
     gainers_list = gainers_info.get_instruments(complete_info=False)
     equity_scraper = EquityScraper()
-    percent_delivered = str(equity_scraper.get_info_all(gainers_list, specific_info_key='deliveryToTradedQuantity'))
-    last_price = str(equity_scraper.get_info_all(gainers_list, specific_info_key='lastPrice'))
-    Logger.info(percent_delivered, push_to_slack=True)
-    Logger.info(last_price, push_to_slack=True)
+    percent_delivered = equity_scraper.get_info_all(gainers_list, specific_info_key='deliveryToTradedQuantity')
+    last_price = equity_scraper.get_info_all(gainers_list, specific_info_key='lastPrice')
+
+    ## Formatting output ##
+    percent_delivered_str = 'NSEIndia - deliveryToTradedQuantity\n'
+    for item in percent_delivered:
+        percent_delivered_str = percent_delivered_str + '{} - {}\n'.format(item, percent_delivered[item])
+
+
+    last_price_str = 'NSEIndia - lastPrice\n'
+    for item in last_price:
+        last_price_str = last_price_str + '{} - {}\n'.format(item, last_price[item])
+
+
+    Logger.info(percent_delivered_str, push_to_slack=False)
+    Logger.info(last_price_str, push_to_slack=False)
 
 
 ##### Rediff money ######
@@ -31,8 +43,20 @@ def rediff_money():
     gainers_list = gainers_info.get_instruments(limit_number_of_instruments=3)
     print(gainers_list)
     equity_scraper = EquityScraper()
-    percent_delivered = str(equity_scraper.get_info_all(gainers_list, specific_info_key='deliveryToTradedQuantity'))
-    last_price = str(equity_scraper.get_info_all(gainers_list, specific_info_key='lastPrice'))
-    Logger.info(percent_delivered, push_to_slack=True)
-    Logger.info(last_price, push_to_slack=True)
+        percent_delivered = equity_scraper.get_info_all(gainers_list, specific_info_key='deliveryToTradedQuantity')
+    last_price = equity_scraper.get_info_all(gainers_list, specific_info_key='lastPrice')
+
+    ## Formatting output ##
+    percent_delivered_str = 'RediffMoney - deliveryToTradedQuantity\n'
+    for item in percent_delivered:
+        percent_delivered_str = percent_delivered_str + '{} - {}\n'.format(item, percent_delivered[item])
+
+
+    last_price_str = 'RediffMoney - lastPrice\n'
+    for item in last_price:
+        last_price_str = last_price_str + '{} - {}\n'.format(item, last_price[item])
+
+
+    Logger.info(percent_delivered_str, push_to_slack=False)
+    Logger.info(last_price_str, push_to_slack=False)
 
