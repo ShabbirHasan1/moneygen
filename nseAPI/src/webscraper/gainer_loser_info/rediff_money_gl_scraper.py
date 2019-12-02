77ab508bd97d160240f654c19902444bb07af15b
Mayank Gupta <techfreakworm@gmail.com>
2019-12-02 16:55:51 +0530

    Scrape base table from rediff money

--- nseAPI/src/webscraper/gainer_loser_info/rediff_money_gl_scraper.py
+++ nseAPI/src/webscraper/gainer_loser_info/rediff_money_gl_scraper.py
@@ -1,4 +1,6 @@
 from webscraper.gainer_loser_info.base_gl_scraper import BaseGLScraper
+from bs4 import BeautifulSoup
+import requests
 
 
 class RediffMoneyGLScraper(BaseGLScraper):
@@ -6,12 +8,31 @@ class RediffMoneyGLScraper(BaseGLScraper):
     Parameters:
     view_type (str): Can only be 'All' or 'Nifty 50'
     '''
-    def __init__(self):
+    def __init__(self, view_type: str):
         super().__init__()
+        view_type_map = {
+            'All':'groupall',
+            'Nifty 50':'nifty'
+        }
+        self.view_type = view_type_map[view_type]
     
 
     def get_instruments(self, complete_info=False):
-        pass
+        url = 'https://money.rediff.com/gainers/nse/daily/' + self.view_type
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        table = soup.findAll("table", attrs={"class": "dataTable"})[0]
+        table_body = table.find('tbody')
+        rows = table_body.find_all('tr')
+        data = []
+        for row in rows:
+            cols = row.find_all('td')
+            link = row.find_all('td')[0].find_all('a')[0]['href']
+            cols = [ele.text.strip() for ele in cols]
+            cols.append('https:'+link)
+            data.append([ele for ele in cols if ele])
+        # TODO: Return Symbols instead of names of instruments and check for complete data flag
+        return data
 
     def get_instrument_info(self):
         pass
