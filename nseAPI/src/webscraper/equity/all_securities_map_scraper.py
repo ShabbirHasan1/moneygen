14eb90855458fb014f3d7ae37c9962cbae9e9fee
Mayank Gupta <techfreakworm@gmail.com>
2019-12-05 12:46:29 +0530

    Add TODO

---
+++ nseAPI/src/webscraper/equity/all_securities_map_scraper.py
@@ -0,0 +1,7 @@
+
+# TODO: Scrape and store all securities map from NSE INDIA website
+# @body
+# Can be scraped from 'https://www.nseindia.com/corporates/content/securities_info.htm'
+class AllSecuritiesMapScraper:
+    def __init__(self):
+        pass
