5e38248420751728e0a22d6465a6cf8c27b91b70
Mayank Gupta <techfreakworm@gmail.com>
2019-12-05 12:41:23 +0530

    Add mongo adapter'

--- nseAPI/src/config.py
+++ nseAPI/src/config.py
@@ -8,3 +8,7 @@ class Config:
     SELENIUM_DRIVER_EXEC_PATH = \
         os.path.join(SELENIUM_DRIVER_BASE_PATH, sys.platform)
     SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
+    MONGO_DB = 'nse_api'
+    MONGO_CONNECTION_STRING = os.getenv('MONGO_CONNECTION_STRING') + '/'
+    CONNECTION_ALIAS = 'nse_api'
+    GAINER_LOSER_COLLECTION = 'gainer_loser_info'
---
+++ nseAPI/src/db/models.py
@@ -0,0 +1,14 @@
+from pymodm import MongoModel, fields
+from pymongo.write_concern import WriteConcern
+from config import Config
+
+
+
+class GainerLoserInfoModel(MongoModel):
+    listOfCompanies = fields.ListField()
+    createdBy = fields.CharField()
+    createdDate = fields.DateTimeField()
+
+    class Meta:
+        write_concern = WriteConcern(j=True)
+        connection_alias = Config.CONNECTION_ALIAS
