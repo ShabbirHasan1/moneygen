f736957829a5e1d1b86279800799ec18f054bfe8
Mayank Gupta <techfreakworm@gmail.com>
2020-01-17 21:42:41 +0530

    Kite job working

--- nseAPI/src/main_kite.py
+++ nseAPI/src/main_kite.py
@@ -1,6 +1,3 @@
-from webscraper.index_historical import IndexHistoricalOptions
-from tasks.traded_to_percent_delivered import TradedToPercentDelivered, TradedToPercentDeliveredReport
-from kite_simulator import SimulationSetup, LiveSimulator
 from util.log import Logger
 from datetime import datetime
 from dateutil.tz import *
@@ -15,22 +12,32 @@ market_open = now.replace(hour=9, minute=15)
 market_close = now.replace(hour=15, minute=30)
 market_preopen_open = now.replace(hour=9, minute=0)
 market_preopen_close = market_open
-
+    
 
 if now >= market_preopen_open and now <= market_open:
+    from kite_simulator import SimulationSetup
     SimulationSetup()
     Logger.info('Kite Job Simulation Setup completed at: ' + str(datetime.now()), push_to_slack=False)
 
 if now >= market_open and now <= market_close:
     # Running in different process, since Websocket reactor cannot be restarted in same process
-    simulator = LiveSimulator()
-    init_process = Process(target=simulator.sim_init)
+    # Mongo CLient should not be created before forking (creating a child process), therefore creating mini functions
+    def simulation_init_process():
+        from kite_simulator import LiveSimulator
+        LiveSimulator().sim_init()
+
+    def simulate_market_process():
+        from kite_simulator import LiveSimulator
+        simulator = LiveSimulator()
+        simulator.simulate_market()
+        simulator.calculate_and_store_pnl()
+
+    init_process = Process(target=simulation_init_process)
     init_process.start()
     init_process.join(timeout=None)
-    simulation_process = Process(target=simulator.simulate_market)
+    simulation_process = Process(target=simulate_market_process)
     simulation_process.start()
     simulation_process.join(timeout=None)
-    simulator.calculate_and_store_pnl()
     Logger.info('Kite Job Simulation and PNL calculation completed at: ' + str(datetime.now()), push_to_slack=False)
 
 Logger.info('Complete!')
