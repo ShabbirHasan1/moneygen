f71cb3d71d60fea637fecd2573cee980e10d2f18
Mayank Gupta <techfreakworm@gmail.com>
2020-04-05 07:39:35 +0530

    Add Restful API methods

--- nseAPI/src/api/core.py
+++ nseAPI/src/api/core.py
@@ -4,7 +4,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask, request, redirect, url_for, abort, jsonify, Response
 from flask_cors import CORS, cross_origin
 from requests_toolbelt.multipart.encoder import MultipartEncoder
-from
+from kite_market.order import OrderManager
 
 
 class PrefixMiddleware:
@@ -25,38 +25,49 @@ class PrefixMiddleware:
 app = Flask(__name__)
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='')
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+order_manager = OrderManager()
 # app = CORS(app)
 
 # Webhook URL
 @cross_origin()
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    pass
-
-# Order management URLs
-@cross_origin()
-@app.route('/order', methods=['GET', 'POST', 'DELETE'])
-def order_get_post_delete():
-    if request.method == 'GET':
-        pass
-    elif request.method == 'POST':
-        pass
-    elif request.method == 'DELETE':
-        pass
+    if request.method == 'POST':
+        update = request.json()
+        status = order_manager.update_order(updated_order=update, order_id=update['order_id'])
+        # Cannot return anything, it's webhook
     else:
         return jsonify({'msg': 'Bad Request'}), 401
 
-@cross_origin()
+# Order management URLs
+# @cross_origin()
+@app.route('/order', methods=['GET', 'POST', 'DELETE'])
+def order_get_post_delete():
+    if request.method == 'GET':
+        result = order_manager.get_order()
+        return jsonify({'data': result}), 200
+    elif request.method == 'POST':
+        result = order_manager.create_order(order_spec=request.json())
+        return jsonify({'data': result}), 200
+    elif request.method == 'DELETE':
+        result = order_manager.delete_order()
+        return jsonify({'data': result}), 200
+    else:
+        return jsonify({'msg': 'Bad Request'}), 401
+
+# @cross_origin()
 @app.route('/order/{order_id}', methods=['GET', 'PUT', 'DELETE', 'PATCH'])
 def get_or_modify_specific_order(order_id: int):
     if request.method == 'GET':
-        pass
-    elif request.method == 'PUT':
-        pass
-    elif request.method == 'PATCH':
-        pass
+        result = order_manager.get_order(order_id=order_id)
+        return jsonify({'data': result}), 200
+    elif request.method == 'PUT' or request.method == 'PATCH':
+        result = order_manager.update_order(updated_order=request.json(), order_id=order_id)
+        return jsonify({'data': result}), 200
     elif request.method == 'DELETE':
-        pass
+        result = order_manager.delete_order(order_id=order_id)
+        return jsonify({'data': result}), 200
     else:
         return jsonify({'msg': 'Bad Request'}), 401
 
