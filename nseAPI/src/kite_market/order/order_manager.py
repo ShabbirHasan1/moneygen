2975dcabb55dd97e4bb9afc985c7544ea9e4b2cc
Mayank Gupta <techfreakworm@gmail.com>
2020-04-05 07:06:18 +0530

    Basic layot for order manager

---
+++ nseAPI/src/kite_market/order/order_manager.py
@@ -0,0 +1,17 @@
+
+
+class OrderManager:
+    def __init__(self):
+        pass
+
+    def create_order(self, order_spec: dict):
+        pass
+
+    def update_order(self, updated_order: dict, order_id=None):
+        pass
+
+    def get_order(self, order_id=None):
+        pass
+
+    def delete_order(self, order_id=None):
+        pass
