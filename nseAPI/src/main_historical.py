bc61bdb43c63a0203982d958a647cd30cbc5f2b7
Mayank Gupta <techfreakworm@gmail.com>
2020-05-11 04:45:25 +0530

    Fix bug

--- nseAPI/src/main_historical.py
+++ nseAPI/src/main_historical.py
@@ -5,12 +5,15 @@ avail = AvailableSecurities()
 
 symbols = avail.get_available_securities(columns=['SYMBOL'], series='EQ')
 
+# Re arrange symbol list
+symbols = [x[0] for x in symbols]
+
 print("All symbol length: " ,len(symbols))
 hist = HistoricalData()
 
 hist.create_table_for_securities(symbols)
 
 for index, symbol in enumerate(symbols):
-    print(f"Security #{index + 1}: {symbol[0]}")
-    df = hist.get_data_for_security(symbol[0])
+    print(f"Security #{index + 1}: {symbol}")
+    df = hist.get_data_for_security(symbol)
     hist.upsert_data(df)
